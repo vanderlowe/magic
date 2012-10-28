2fdc2b885c4286fcccedcca11e29c065813c7c33
Ilmo van der Lowe <github@vanderlowe.com>
2012-10-28 19:57:43 +0000

    Added RODBC requirement to sql function

--- R/sql.R
+++ R/sql.R
@@ -1,5 +1,9 @@
 sql <- function(database = NULL, query = NULL, dsn = "azure", driver = "MySQL") {
-	# This function assumes that you have set a dsn named 'azure' and installed ODBC drivers for MySQL
+  # This function assumes that you have set a dsn named 'azure' and installed ODBC drivers for MySQL
+  
+  lib <- require("RODBC")
+  if (!lib) {stop("You must have package 'RODBC' installed.")}
+	
 	if (is.null(database)) {stop("You must specify which database you want to use.")}
 	if (is.null(query)) {stop("You must specify an SQL query")}
 
