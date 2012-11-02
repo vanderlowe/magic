31e7a90983a1c75494bd36dd202bb192f82375bc
Ilmo van der Lowe <github@vanderlowe.com>
2012-11-02 20:35:56 +0000

    Documentation to sql; 1-column results converted to vectors

--- R/sql.R
+++ R/sql.R
@@ -1,9 +1,7 @@
-sql <- function(database = NULL, query = NULL, dsn = "azure") {
+sql <- function(database = "", query = "SHOW DATABASES;", dsn = "azure") {
   
   lib <- require("RODBC")
   if (!lib) {stop("You must have package 'RODBC' installed.")}
-	
-	if (is.null(database)) {stop("You must specify which database you want to use.")}
 	if (is.null(query)) {stop("You must specify an SQL query")}
 
 	# Create database connection
@@ -12,6 +10,7 @@ sql <- function(database = NULL, query = NULL, dsn = "azure") {
 
 	# Execute query
 	result <- sqlQuery(db, query)
+  if (ncol(result) == 1) {result <- result[,1]}
 
 	# Close unused connection
 	odbcClose(db)
