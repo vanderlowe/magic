ca9309834190cf38bf33104643ecb1843f60011a
Ilmo van der Lowe <github@vanderlowe.com>
2013-05-30 13:49:33 +0100

    Citation information

---
+++ inst/CITATION.R
@@ -0,0 +1,16 @@
+bibentry("Manual",
+         title = "magic",
+         author = person(given = "Ilmo", 
+                         middle = "Konstantin", 
+                         family= "Löwe"),
+         organization = "Cambridge Prosociality and Well-being Lab, University of Cambridge",
+         address      = "Cambridge, United Kingdom",
+         year   = 2013,
+         url    = "https://github.com/vanderlowe/magic",
+         
+         mheader = "To cite magic in publications use:",
+         
+         mfooter = 
+           paste("I have invested a lot of time and effort in creating magic,",
+                 "please cite it when using it for data analysis.", sep = " ")
+)
