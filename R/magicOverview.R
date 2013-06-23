8ef352ccdbbc053764e471120ac6d3e50fbf60c5
Ilmo van der Lowe <github@vanderlowe.com>
2013-06-23 09:23:31 +0100

    Finalized magicOverview

--- R/magicOverview.R
+++ R/magicOverview.R
@@ -1,30 +1,38 @@
 #' Visual map of literature
 #' 
 #' This function queries the \code{cpw_litReview} to visualize a citation network between the papers in the database.
+#' The line color indicates the section in which the citation was made: blue for introduction, green for methods, red for results, and gray for discussion. The lines are partially transparent, so color blends indicate citations in multiple sections.
 #' @export
 #' @import igraph
 #' @param user Optional vector of user names to filter citations based on who contributed them into the database.
-magicOverview <- function(user = NULL, simplify = TRUE) {
+#' @param min.citations An integer to indicate how many times a paper has to be cited in order to be included in the graph (3 by default). Use 0 to plot the entire network as-is.
+magicOverview <- function(user = NULL, min.citations = 3) {
   g <- magicCitationNetwork(user = user)
-  if (simplify) {
+  if (simplify > 0) {
+    citers <- V(delete.vertices(g, which(degree(g, mode = "out") < 1)))$name
+    
     # Remove nodes with no edges
-    new_g <- delete.vertices(g, which(degree(g) < 1)-1)
+    g <- delete.vertices(g, which(degree(g, mode = "in") < simplify & !V(g)$name %in% citers))
   }
   
-  prettyNetwork(g)
+  magic:::prettyNetwork(g)
 }
 
 prettyNetwork <- function(g) {
+  # The order for colors is: discussion (gray), intro (blue), methods (green), results (red)
+  colors <- c("#CCCCCCBB", "#0000FFBB", "#00FF00BB", "#FF0000BB")
   igraph::plot.igraph(g,
                       layout = layout.fruchterman.reingold,
                       vertex.size = 0,
                       vertex.color = NA,
                       vertex.frame.color = NA,
                       vertex.label = V(g)$name,
-                      vertex.label.cex = 0.5,
+                      vertex.label.cex = 0.6,
                       vertex.label.family = "sans",
                       vertex.label.dist = 0.1,
-                      edge.color = factor(E(g)$section),
+                      vertex.label.degree = -pi/2,
+                      edge.width = 1.5,
+                      edge.color = colors[factor(E(g)$section)],
                       edge.arrow.size = 0.1,
                       edge.arrow.width = 0.1,
                       edge.curved = F
