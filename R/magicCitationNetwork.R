04250635b30ed31c17db9bc6b32395a021ab1d36
Ilmo van der Lowe <github@vanderlowe.com>
2013-06-24 15:04:37 +0100

    Fixed bug in user agrument

--- R/magicCitationNetwork.R
+++ R/magicCitationNetwork.R
@@ -7,8 +7,11 @@ magicCitationNetwork <- function(user = NULL) {
   if (is.null(user)) {
     users.cit <- magicSQL("SELECT DISTINCT(user) FROM citations", "cpw_litReview")$user
     users.pap <- magicSQL("SELECT DISTINCT(user) FROM papers", "cpw_litReview")$user
-    users <- unique(c(users.cit, users.pap))
+  } else {
+    users.cit <- magicSQL(sprintf("SELECT DISTINCT(user) FROM citations WHERE user IN ('%s')", paste(user, collapse = "', '")), "cpw_litReview")$user
+    users.pap <- magicSQL(sprintf("SELECT DISTINCT(user) FROM papers WHERE user IN ('%s')", paste(user, collapse = "', '")), "cpw_litReview")$user
   }
+  users <- unique(c(users.cit, users.pap))
   
   # Citations are edges in the network
   citations <- magicSQL(sprintf("SELECT `from`, `to`, `section` FROM citations WHERE user IN ('%s') AND `to` IS NOT NULL", paste(users, collapse = "','")), "cpw_litReview")
