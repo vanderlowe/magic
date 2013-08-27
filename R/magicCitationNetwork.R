ea74b7792fb7ba48b547df6aea0a6c42facdf057
Ilmo van der Lowe <github@vanderlowe.com>
2013-08-27 12:05:43 +0100

    Fixing possible Mac/PC incompatibility

--- R/magicCitationNetwork.R
+++ R/magicCitationNetwork.R
@@ -5,11 +5,11 @@
 #' @import igraph
 magicCitationNetwork <- function(user = NULL) {
   if (is.null(user)) {
-    users.cit <- magicSQL("SELECT DISTINCT(user) FROM citations", "cpw_litReview")$user
-    users.pap <- magicSQL("SELECT DISTINCT(user) FROM papers", "cpw_litReview")$user
+    users.cit <- as.character(magicSQL("SELECT DISTINCT(user) FROM citations", "cpw_litReview")$user)
+    users.pap <- as.character(magicSQL("SELECT DISTINCT(user) FROM papers", "cpw_litReview")$user)
   } else {
-    users.cit <- magicSQL(sprintf("SELECT DISTINCT(user) FROM citations WHERE user IN ('%s')", paste(user, collapse = "', '")), "cpw_litReview")$user
-    users.pap <- magicSQL(sprintf("SELECT DISTINCT(user) FROM papers WHERE user IN ('%s')", paste(user, collapse = "', '")), "cpw_litReview")$user
+    users.cit <- as.character(magicSQL(sprintf("SELECT DISTINCT(user) FROM citations WHERE user IN ('%s')", paste(user, collapse = "', '")), "cpw_litReview")$user)
+    users.pap <- as.character(magicSQL(sprintf("SELECT DISTINCT(user) FROM papers WHERE user IN ('%s')", paste(user, collapse = "', '")), "cpw_litReview")$user)
   }
   users <- unique(c(users.cit, users.pap))
   
